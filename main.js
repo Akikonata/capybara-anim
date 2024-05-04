47da811910d08f8c916bef13982592df3327e4c1
panzheng <panzheng@baidu.com>
2024-05-05 02:44:18 +0800

    加上骆驼动画切换

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0
 // 创建一个简单的用户界面
 scene.createDefaultCameraOrLight(true);
 scene.activeCamera.attachControl(canvas, false);
- 
+let camel = null;
 // 加载glb文件
 BABYLON.SceneLoader.ImportMesh("", "camel/idle.glb", "", scene, function (newMeshes) {
+    camel?.dispose();
+    camel = newMeshes[0];
     // 这里可以处理加载后的Mesh，例如调整尺寸、位置等
     newMeshes[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
     newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
@@ -35,28 +37,37 @@ window.addEventListener("resize", function () {
     canvas.height = window.innerHeight;
     engine.resize();
 });
+
 // 监听postMessage信息并更换骆驼模型
 window.addEventListener("message", function (event) {
     if (event.data === "walk") {
         BABYLON.SceneLoader.ImportMesh("", "camel/walk.glb", "", scene, function (newMeshes) {
+            camel?.dispose();
+            camel = newMeshes[0];
             newMeshes[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
             newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
             newMeshes[0].position = new BABYLON.Vector3(0, -0.5, 0);
         });
     } else if (event.data === "run") {
         BABYLON.SceneLoader.ImportMesh("", "camel/run.glb", "", scene, function (newMeshes) {
+            camel?.dispose();
+            camel = newMeshes[0];
             newMeshes[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
             newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
             newMeshes[0].position = new BABYLON.Vector3(0, -0.5, 0);
         });
     } else if (event.data === "dead") {
         BABYLON.SceneLoader.ImportMesh("", "camel/dead.glb", "", scene, function (newMeshes) {
+            camel?.dispose();
+            camel = newMeshes[0];
             newMeshes[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
             newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
             newMeshes[0].position = new BABYLON.Vector3(0, -0.5, 0);
         });
     } else {
         BABYLON.SceneLoader.ImportMesh("", "camel/idle.glb", "", scene, function (newMeshes) {
+            camel?.dispose();
+            camel = newMeshes[0];
             newMeshes[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
             newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
             newMeshes[0].position = new BABYLON.Vector3(0.5, -0.5, 0);
