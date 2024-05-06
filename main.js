2d1b05869ab41662d99301ec27043ba9bdf54a73
panzheng <panzheng@baidu.com>
2024-05-06 10:40:10 +0800

    添加 loading 动画

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const engine = new BABYLON.Engine(canvas, true);
 const scene = new BABYLON.Scene(engine);
  
 // 添加相机和光源
-const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
+const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / -1, Math.PI / 2.5, 13, new BABYLON.Vector3(0, 0, 0), scene);
 const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
  
 // 创建一个简单的用户界面
@@ -31,25 +31,25 @@ window.addEventListener("resize", function () {
     engine.resize();
 });
 
-// {
-//     "action": "死掉",
-//     "description": "骆驼已经彻底躺平，不再动弹。",
-//     "emotion": "坚定",
-//     "movement": "0"
-// }
 function initCamel() {
     const url = new URL(window.location.href);
     const camelAction = url.searchParams.get("action");
     const camelDescription = url.searchParams.get("description");
     const camelEmotion = url.searchParams.get("emotion");
     const camelMovement = url.searchParams.get("movement");
-    BABYLON.SceneLoader.ImportMesh("", `camel/${camelAction || 'walk'}.glb`, "", scene, function (newMeshes) {
-        camel?.dispose();
-        camel = newMeshes[0];
-        newMeshes[0].scaling = new BABYLON.Vector3(0.25, 0.25, 0.25);
-        newMeshes[0].rotation = new BABYLON.Vector3(0, -Math.PI / 2, 0);
-        newMeshes[0].position = new BABYLON.Vector3(0.1, -0.5, 0);
-    });
     let descriptionElement = document.getElementById("camelDescription");
-    descriptionElement.innerHTML = camelDescription || "你的牛马积极打工中";
+    descriptionElement.innerHTML = camelDescription || "牛马打工中";
+    BABYLON.SceneLoader.AppendAsync(
+        `camel/${camelAction || 'walk'}.glb`,
+        undefined,
+        scene,
+        function (event) {
+          const percentage = event.lengthComputable ? " " + Math.floor((event.loaded / event.total) * 100) + "%" : "";
+          descriptionElement.innerHTML = "你的牛马正在积极拥抱变化" + percentage;
+        },
+        ".glb",
+    ).then((scene, message) => {
+        console.log(scene, message);
+        descriptionElement.innerHTML = camelDescription || "牛马打工中";
+    });
 }
